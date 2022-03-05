3661fe79a5245dfa0ae839aff40de1261a7a87b3
Ivan Uemlianin <ivan@llaisdy.com>
2022-03-05 22:29:46 +0000

    name change

--- clingo-mode.el
+++ clingo-mode.el
@@ -1,20 +1,29 @@
-;;; pasp-mode.el ---- A major mode for editing Answer Set Programs. -*- lexical-binding: t -*-
+;;; clingo-mode.el --- A major mode for editing Answer Set Programs -*- lexical-binding: t -*-
 
 ;; Copyright (c) 2020 by Ivan Uemlianin
 ;; Copyright (c) 2017 by Henrik Jürges
 
 ;; Author: Ivan Uemlianin <ivan@llaisdy.com>
-;; URL: https://github.com/llaisdy/pasp-mode
-;; Version: 0.3.0
+;; URL: https://github.com/llaisdy/clingo-mode
+;; Version: 0.4.0
 ;; Author: Henrik Jürges <juerges.henrik@gmail.com>
 ;; URL: https://github.com/santifa/pasp-mode
 ;; Version: 0.1.0
 ;; Package-requires: ((emacs "24.3"))
-;; Keywords: asp, pasp, Answer Set Programs, Potassco Answer Set Programs, Major mode, languages
+;; Keywords: asp, clingo, Answer Set Programs, Potassco, Major mode, languages
 
-;; You can redistribute this program and/or modify it under the terms of the GNU
-;; General Public License as published by the Free Software Foundation; either
-;; version 3 of the License, or (at your option) any later version..
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
@@ -29,14 +38,14 @@
 ;;; Install
 
 ;; Open the file with Emacs and run "M-x eval-buffer"
-;; Open an ASP file and run "M-x pasp-mode"
+;; Open an ASP file and run "M-x clingo-mode"
 
 ;; To manually load this file within your Emacs config
 ;; add this file to your load path and place
-;; (require 'pasp-mode)
+;; (require 'clingo-mode)
 ;; in your init file.
 
-;; See "M-x customize-mode pasp-mode" for information
+;; See "M-x customize-mode clingo-mode" for information
 ;; about mode configuration.
 
 ;;; Features
@@ -78,51 +87,52 @@
 
 ;;; Customization
 
-(defgroup pasp-mode nil
+(defgroup clingo nil
   "Major mode for editing Anwser Set Programs."
   :group 'languages
-  :prefix "pasp-")
+  :prefix "clingo-")
 
-(defcustom pasp-mode-version "0.3.0"
-  "Version of `pasp-mode'."
-  :group 'pasp-mode)
+(defcustom clingo-mode-version "0.4.0"
+  "Version of `clingo-mode'."
+  :type 'string
+  :group 'clingo-mode)
 
-(defcustom pasp-indentation 2
+(defcustom clingo-indentation 2
   "Level of indentation."
   :type 'integer
-  :group 'pasp-mode)
+  :group 'clingo-mode)
 
-(defcustom pasp-clingo-path (executable-find "clingo")
+(defcustom clingo-path (executable-find "clingo")
   "Path to clingo binary used for execution."
   :type 'string
-  :group 'pasp-mode)
+  :group 'clingo-mode)
 
-(defcustom pasp-clingo-options ""
+(defcustom clingo-options ""
   "Command line options passed to clingo."
   :type 'string
-  :group 'pasp-mode
+  :group 'clingo-mode
   :safe #'stringp)
 
-(defcustom pasp-pretty-symbols-p t
+(defcustom clingo-pretty-symbols-p t
   "Use Unicode characters where appropriate."
   :type 'boolean
-  :group 'pasp-mode)
+  :group 'clingo-mode)
 
 ;;; Pretty Symbols
 
-(defvar pasp-mode-hook
+(defvar clingo-mode-hook
   (lambda ()
-    (when pasp-pretty-symbols-p
+    (when clingo-pretty-symbols-p
       (push '(":-" . ?⊢) prettify-symbols-alist)
       (push '(">=" . ?≥) prettify-symbols-alist)
       (push '("<=" . ?≤) prettify-symbols-alist)
       (push '("!=" . ?≠) prettify-symbols-alist)
-     (push '("not" . ?¬) prettify-symbols-alist))))
+      (push '("not" . ?¬) prettify-symbols-alist))))
 
 ;;; Syntax table
 
-(defvar pasp-mode-syntax-table nil "Syntax table for `pasp-mode`.")
-(setq pasp-mode-syntax-table
+(defvar clingo-mode-syntax-table nil "Syntax table for `clingo-mode`.")
+(setq clingo-mode-syntax-table
       (let ((table (make-syntax-table)))
         ;; modify syntax table
         (modify-syntax-entry ?' "w" table)
@@ -133,74 +143,74 @@
 
 ;;; Syntax highlighting faces
 
-(defvar pasp-atom-face 'pasp-atom-face)
-(defface pasp-atom-face
+(defvar clingo-atom-face 'clingo-atom-face)
+(defface clingo-atom-face
   '((t (:inherit font-lock-keyword-face :weight normal)))
   "Face for ASP atoms (starting with lower case)."
   :group 'font-lock-highlighting-faces)
 
-(defvar pasp-construct-face 'pasp-construct-face)
-(defface pasp-construct-face
+(defvar clingo-construct-face 'clingo-construct-face)
+(defface clingo-construct-face
   '((default (:inherit font-lock-builtin-face :height 1.1)))
   "Face for ASP base constructs."
   :group 'font-lock-highlighting-faces)
 
 ;; Syntax highlighting
 
-(defvar pasp--constructs
-  '("\\.\\|:-\\|:\\|;\\|:~\\|,\\|(\\|)\\|{\\|}\\|[\\|]\\|not " . pasp-construct-face)
+(defvar clingo--constructs
+  '("\\.\\|:-\\|:\\|;\\|:~\\|,\\|(\\|)\\|{\\|}\\|[\\|]\\|not " . clingo-construct-face)
    "ASP constructs.")
 
-(defconst pasp--constant
+(defconst clingo--constant
   '("#[[:word:]]+" . font-lock-builtin-face)
   "ASP constants.")
 
-(defconst pasp--variable
+(defconst clingo--variable
   '("[^[:word:]]\\(_*\\([[:upper:]][[:word:]_']*\\)?\\)" . (1 font-lock-variable-name-face))
   "ASP variable.")
 
-(defconst pasp--variable2
+(defconst clingo--variable2
   '("\\_<\\(_*[[:upper:]][[:word:]_']*\\)\\_>" . (1 font-lock-variable-name-face))
   "ASP variable 2.")
 
-(defconst pasp--atom
-  '("_*[[:lower:]][[:word:]_']*" . pasp-atom-face)
+(defconst clingo--atom
+  '("_*[[:lower:]][[:word:]_']*" . clingo-atom-face)
   "ASP atoms.")
 
-(defvar pasp-highlighting nil
+(defvar clingo-highlighting nil
   "Regex list for syntax highlighting.")
-(setq pasp-highlighting
+(setq clingo-highlighting
       (list
-       pasp--constructs
-       pasp--constant
-       pasp--variable2
-       pasp--variable
-       pasp--atom))
+       clingo--constructs
+       clingo--constant
+       clingo--variable2
+       clingo--variable
+       clingo--atom))
 
 ;;; Compilation
 
-(defvar pasp-error-regexp
+(defvar clingo-error-regexp
   "^[  ]+at \\(?:[^\(\n]+ \(\\)?\\(\\(?:[a-zA-Z]:\\)?[a-zA-Z\.0-9_/\\-]+\\):\\([0-9]+\\):\\([0-9]+\\)\)?"
   "Taken from NodeJS -> only dummy impl.")
 
-(defvar pasp-error-regexp-alist
-  `((,pasp-error-regexp 1 2 3))
+(defvar clingo-error-regexp-alist
+  `((,clingo-error-regexp 1 2 3))
   "Taken from NodeJs -> only dummy impl.")
 
-(defun pasp-compilation-filter ()
+(defun clingo-compilation-filter ()
   "Filter clingo output.  (Only dummy impl.)."
   (ansi-color-apply-on-region compilation-filter-start (point-max))
   (save-excursion
     (while (re-search-forward "^[\\[[0-9]+[a-z]" nil t)
       (replace-match ""))))
 
-(define-compilation-mode pasp-compilation-mode "ASP"
+(define-compilation-mode clingo-compilation-mode "ASP"
   "Major mode for running ASP files."
   (progn
-    (set (make-local-variable 'compilation-error-regexp-alist) pasp-error-regexp-alist)
-    (add-hook 'compilation-filter-hook 'pasp-compilation-filter nil t)))
+    (set (make-local-variable 'compilation-error-regexp-alist) clingo-error-regexp-alist)
+    (add-hook 'compilation-filter-hook 'clingo-compilation-filter nil t)))
 
-(defun pasp-generate-command (encoding options &optional instance)
+(defun clingo-generate-command (encoding options &optional instance)
   "Generate Clingo call with some ASP input file.
 
 Argument ENCODING The current buffer which holds the problem encoding.
@@ -208,13 +218,13 @@ Argument OPTIONS Options (possibly empty string) sent to clingo.
 Optional argument INSTANCE The problem instance which is solved by the encoding.
   If no instance it is assumed to be also in the encoding file."
   (if 'instance
-      (concat pasp-clingo-path " " options " " encoding " " instance)
-    (concat pasp-clingo-path " " options " " encoding)))
+      (concat clingo-path " " options " " encoding " " instance)
+    (concat clingo-path " " options " " encoding)))
 
-(defun pasp-run-clingo (encoding options &optional instance)
+(defun clingo-run-clingo (encoding options &optional instance)
   "Run Clingo with some ASP input files.
-Be aware: Partial ASP code may lead to abnormally exits while
-the result is sufficient.
+Be aware: Partial ASP code may lead to abnormal exits
+  while the result is sufficient.
 
 Argument ENCODING The current buffer which holds the problem encoding.
 Argument OPTIONS Options (possibly empty string) sent to clingo.
@@ -222,11 +232,11 @@ Optional argument INSTANCE The problem instance which is solved by the encoding.
   If no instance it is assumed to be also in the encoding file."
   (when (get-buffer "*clingo output*")
     (kill-buffer "*clingo output*"))
-  (let ((test-command-to-run (pasp-generate-command encoding options instance))
+  (let ((test-command-to-run (clingo-generate-command encoding options instance))
         (compilation-buffer-name-function (lambda (_) "" "*clingo output*")))
-    (compile test-command-to-run 'pasp-compilation-mode)))
+    (compile test-command-to-run 'clingo-compilation-mode)))
 
-(defun pasp-generate-echo (region options &optional instance)
+(defun clingo-generate-echo (region options &optional instance)
   "Generate Clingo call with region echoed to it.
 
 Argument REGION The selected region which holds the problem encoding.
@@ -234,97 +244,101 @@ Argument OPTIONS Options (possibly empty string) sent to clingo.
 Optional argument INSTANCE The problem instance which is solved by the encoding.
   If no instance it is assumed to be also in the encoding file."
   (if 'instance
-      (concat "echo \"" region "\" | " pasp-clingo-path " " options " " instance)
-    (concat "echo \"" region "\" | " pasp-clingo-path " " options)))
+      (concat "echo \"" region "\" | " clingo-path " " options " " instance)
+    (concat "echo \"" region "\" | " clingo-path " " options)))
 
-;; (defun pasp-echo-clingo (region-begin region-end options &optional instance)
-(defun pasp-echo-clingo (region options &optional instance)
+;; (defun clingo-echo-clingo (region-begin region-end options &optional instance)
+(defun clingo-echo-clingo (region options &optional instance)
   "Run Clingo on selected region (prompts for options).
 
-Argument REGION The selected region as a string, which holds the problem encoding.
+Argument REGION The selected region as a string, which has the problem encoding.
 Argument OPTIONS Options (possibly empty string) sent to clingo.
 Optional argument INSTANCE The problem instance which is solved by the encoding.
   If no instance it is assumed to be also in the encoding file."
   (when (get-buffer "*clingo output*")
     (kill-buffer "*clingo output*"))
-  (let ((test-command-to-run (pasp-generate-echo region options instance))
+  (let ((test-command-to-run (clingo-generate-echo region options instance))
         (compilation-buffer-name-function (lambda (_) "" "*clingo output*")))
-    (compile test-command-to-run 'pasp-compilation-mode)))
+    (compile test-command-to-run 'clingo-compilation-mode)))
 
 ;; save the last user input
-(defvar pasp-last-instance "")
-(defvar pasp-last-options "")
+(defvar clingo-last-instance "")
+(defvar clingo-last-options "")
 
 ;;;###autoload
-(defun pasp-run-region (region-beginning region-end options)
-  "Run clingo with the selected REGION as input; prompts for OPTIONS."
+(defun clingo-run-region (region-beginning region-end options)
+  "Run Clingo on selected region (prompts for options).
+
+Argument REGION-BEGINNING point marking beginning of region.
+Argument REGION-END point marking end of region.
+Argument OPTIONS Options (possibly empty string) sent to clingo."
   (interactive
    (let ((string
-          (read-string (format "Options [%s]: " pasp-last-options) nil nil pasp-last-options)))
+          (read-string (format "Options [%s]: " clingo-last-options) nil nil clingo-last-options)))
      (list (region-beginning) (region-end) string)))
-  (setq-local pasp-last-options options)
-  (pasp-echo-clingo
+  (setq-local clingo-last-options options)
+  (clingo-echo-clingo
    (buffer-substring-no-properties region-beginning region-end)
    options))
 
 ;;;###autoload
-(defun pasp-run-buffer (options)
+(defun clingo-run-buffer (options)
   "Run clingo with the current buffer as input; prompts for OPTIONS."
   (interactive
-   (list (read-string (format "Options [%s]: " pasp-last-options) nil nil pasp-last-options)))
-  (setq-local pasp-last-options options)
-  (pasp-run-clingo (buffer-file-name) options))
+   (list (read-string (format "Options [%s]: " clingo-last-options) nil nil clingo-last-options)))
+  (setq-local clingo-last-options options)
+  (clingo-run-clingo (buffer-file-name) options))
 
 ;;;###autoload
-(defun pasp-run (options instance)
+(defun clingo-run (options instance)
   "Run clingo with the current buffer and some user provided INSTANCE as input; prompts for OPTIONS."
   (interactive
    (list
-    (read-string (format "Options [%s]: " pasp-last-options) nil nil pasp-last-options)
+    (read-string (format "Options [%s]: " clingo-last-options) nil nil clingo-last-options)
     (read-file-name
-     (format "Instance [%s]:" (file-name-nondirectory pasp-last-instance))
-     nil pasp-last-instance)))
-  (setq-local pasp-last-options options)
-  (setq-local pasp-last-instance instance)
-  (pasp-run-clingo (buffer-file-name) options instance))
+     (format "Instance [%s]:" (file-name-nondirectory clingo-last-instance))
+     nil clingo-last-instance)))
+  (setq-local clingo-last-options options)
+  (setq-local clingo-last-instance instance)
+  (clingo-run-clingo (buffer-file-name) options instance))
 
 ;;; Utility functions
 
-(defun pasp-reload-mode ()
-    "Reload the PASP major mode."
+(defun clingo-reload-mode ()
+    "Reload the CLINGO major mode."
   (interactive)
   (progn
-    (unload-feature 'pasp-mode)
-    (require 'pasp-mode)
-    (pasp-mode)))
+    (unload-feature 'clingo-mode)
+    (require 'clingo-mode)
+    (clingo-mode)))
 
 ;;; File ending
 
 ;;;###autoload
-(add-to-list 'auto-mode-alist '("\\.lp\\'" . pasp-mode))
+(add-to-list 'auto-mode-alist '("\\.lp\\'" . clingo-mode))
 
-;;; Define pasp mode
+;;; Define clingo mode
 
 ;;;###autoload
-(define-derived-mode pasp-mode prolog-mode "Potassco ASP"
+(define-derived-mode clingo-mode prolog-mode "Potassco ASP"
   "A major mode for editing Answer Set Programs."
-  (setq font-lock-defaults '(pasp-highlighting))
+  (setq font-lock-defaults '(clingo-highlighting))
 
   ;; define the syntax for un/comment region and dwim
   (setq-local comment-start "%")
   (setq-local comment-end "")
-  (setq-local tab-width pasp-indentation))
+  (setq-local tab-width clingo-indentation))
 
 ;;; Keymap
 
-(define-key pasp-mode-map (kbd "C-c C-c") 'comment-region)
-(define-key pasp-mode-map (kbd "C-c C-u") 'uncomment-region)
+(define-key clingo-mode-map (kbd "C-c C-c") 'comment-region)
+(define-key clingo-mode-map (kbd "C-c C-u") 'uncomment-region)
 
-(define-key pasp-mode-map (kbd "C-c C-b") 'pasp-run-buffer)
-(define-key pasp-mode-map (kbd "C-c C-r") 'pasp-run-region)
-(define-key pasp-mode-map (kbd "C-c C-e") 'pasp-run)
+(define-key clingo-mode-map (kbd "C-c C-b") 'clingo-run-buffer)
+(define-key clingo-mode-map (kbd "C-c C-r") 'clingo-run-region)
+(define-key clingo-mode-map (kbd "C-c C-e") 'clingo-run)
 
 ;; add mode to feature list
-(provide 'pasp-mode)
+(provide 'clingo-mode)
 
-;;; pasp-mode.el ends here
+;;; clingo-mode.el ends here
